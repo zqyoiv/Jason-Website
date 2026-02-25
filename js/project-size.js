a5bdbb3da18f8061307b998a82948a920dc5c40c
vio <vioyqz.cmu@gmail.com>
2026-02-24 23:25:08 -0500

    fix video spacing

--- js/project-size.js
+++ js/project-size.js
@@ -4,38 +4,77 @@
  * Exposes getProjectPageVideoSize on window for project.js.
  */
 (function () {
-  function getProjectPageVideoSize() {
+  function getContentWidth() {
     var projectPage = document.querySelector('main.project-page');
     var widthPx = 1200;
     var vw = typeof window !== 'undefined' ? window.innerWidth : widthPx;
-    var vh = typeof window !== 'undefined' ? window.innerHeight : 0;
-    var isPortraitMobile = vw <= 768 && vh > 0 && vh > vw;
-
     if (projectPage) {
       var style = window.getComputedStyle(projectPage);
       var paddingLeft = parseFloat(style.paddingLeft) || 0;
       var paddingRight = parseFloat(style.paddingRight) || 0;
-      var contentWidth = projectPage.offsetWidth - paddingLeft - paddingRight;
-      widthPx = contentWidth;
-      if (isPortraitMobile && vw > 0) {
-        var viewportWidth = vw - paddingLeft - paddingRight;
-        if (viewportWidth > widthPx) widthPx = viewportWidth;
-      }
-    } else if (isPortraitMobile && vw > 0) {
+      widthPx = projectPage.offsetWidth - paddingLeft - paddingRight;
+    } else if (vw > 0) {
       widthPx = Math.min(vw, 1200);
     }
+    return widthPx;
+  }
 
-    var aspectRatio = isPortraitMobile ? (9 / 16) : (16 / 9);
-    var heightPx = Math.round(widthPx / aspectRatio);
+  function getProjectPageVideoSize() {
+    var widthPx = getContentWidth();
+    var heightPx = Math.round(widthPx * (9 / 16));
     return { width: widthPx, height: heightPx };
   }
 
   function updateVimeoIframeSizes() {
-    var size = getProjectPageVideoSize();
+    var vw = typeof window !== 'undefined' ? window.innerWidth : 1200;
+    var vh = typeof window !== 'undefined' ? window.innerHeight : 800;
+    var isDesktop = vw > vh;
+    var isMobile = vh >= vw;
+    var contentWidth = getContentWidth();
     var iframes = document.querySelectorAll('.vimeo-iframe');
     for (var i = 0; i < iframes.length; i++) {
-      iframes[i].setAttribute('width', String(size.width));
-      iframes[i].setAttribute('height', String(size.height));
+      var iframe = iframes[i];
+      var orientation = iframe.getAttribute('data-orientation');
+      iframe.classList.remove('landscape-video', 'portrait-video');
+      if (orientation === 'portrait') {
+        iframe.classList.add('portrait-video');
+      } else {
+        iframe.classList.add('landscape-video');
+      }
+      var portrait = orientation === 'portrait';
+      var widthPx, heightPx;
+      var useInlineSize = false;
+
+      if (isDesktop) {
+        if (portrait) {
+          var header = document.querySelector('.header');
+          var headerHeight = header ? header.offsetHeight : 0;
+          heightPx = Math.floor(vh - headerHeight);
+          widthPx = Math.round(heightPx * (9 / 16));
+          useInlineSize = true;
+        } else {
+          widthPx = contentWidth;
+          heightPx = Math.round(widthPx * (9 / 16));
+        }
+      } else {
+        if (portrait) {
+          widthPx = contentWidth;
+          heightPx = Math.round(widthPx * (16 / 9));
+        } else {
+          widthPx = contentWidth;
+          heightPx = Math.round(widthPx * (9 / 16));
+        }
+      }
+
+      iframe.setAttribute('width', String(widthPx));
+      iframe.setAttribute('height', String(heightPx));
+      if (useInlineSize) {
+        iframe.style.setProperty('width', widthPx + 'px', 'important');
+        iframe.style.setProperty('height', heightPx + 'px', 'important');
+      } else {
+        iframe.style.removeProperty('width');
+        iframe.style.removeProperty('height');
+      }
     }
   }
 
@@ -79,11 +118,12 @@
     observeContainer();
   }
 
-  // When crossing mobile/desktop breakpoint (e.g. device preset change), recalc immediately.
+  // When crossing mobile/desktop breakpoint or orientation change, recalc immediately.
   var mq = typeof window.matchMedia !== 'undefined' && window.matchMedia('(max-width: 768px)');
   if (mq && mq.addEventListener) {
-    mq.addEventListener('change', function () {
-      updateVimeoIframeSizes();
-    });
+    mq.addEventListener('change', function () { updateVimeoIframeSizes(); });
   }
+  window.addEventListener('orientationchange', function () {
+    setTimeout(updateVimeoIframeSizes, 100);
+  });
 })();
